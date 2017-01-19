5df7c2029bf9b10b2b389e04715ca7bf2a809ee7
srijit <srijitpaul@gmail.com>
2017-01-19 16:38:29 +0100

    Added more measurements

--- src/cpu_make_script.sh
+++ src/cpu_make_script.sh
@@ -19,8 +19,8 @@ for i in $(seq $START_ARRAY_SIZE $MAX_ARRAY_SIZE);
 	do
 		for j in $(seq $START_RUN $INCREMENT $MAX_RUNS); 
 			do
-				bsub -n 1 -I -R "select [ngpus>0] rusage[ngpus_shared=1]"  ./../bin/saxpy_cputime $i $j $NEVAL>temp.dat 
-                                sed -i '1d' temp.dat
+				bsub -n 1 -o temp.dat -R "select [ngpus>0] rusage[ngpus_shared=1]"  ./../bin/saxpy_cputime $i $j $NEVAL
+                                sed -i -n '1p' temp.dat
                                 cat temp.dat >>../measurements/cpu_timings/${i}.dat			
 
 				#bsub -n 1 -o ../measurements/cpu_CUPTI/$i.out -R "select [ngpus>0] rusage[ngpus_shared=1]"  nvprof --metrics achieved_occupancy,dram_read_throughput,dram_utilization,dram_write_throughput,l2_read_throughput,l2_write_throughput,issued_ipc ./../bin/saxpy_cputime $i $NEVAL
