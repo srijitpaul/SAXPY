ec0b7969a783380c3f690918ab5263894ff14926
srijit <srijitpaul@gmail.com>
2017-05-15 20:31:50 +0200

    Added CUPTI measurements

--- src/make_script.sh
+++ src/make_script.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+nvcc -arch=sm_60 saxpy.cu -o ../bin/saxpy_high
+
+rm  -rf ../measurements/timing/*.out
 END=25
-NEVAL=10
+NEVAL=100
 #Make Bandwidth and execution time measurements
 for i in $(seq 10 $END); 
 	do
 		mkdir -p ../measurements/timings
 		mkdir -p ../measurements/CUPTI
-		bsub -n 1 -o ../measurements/timing/$i.out -R "select [ngpus>0] rusage[ngpus_shared=1]"  ./../bin/saxpy_many $i $NEVAL  
-		bsub -n 1 -o ../measurements/CUPTI/$i.out -R "select [ngpus>0] rusage[ngpus_shared=1]"  nvprof --metrics achieved_occupancy,dram_read_throughput,dram_utilization,dram_write_throughput,l2_read_throughput,l2_write_throughput,issued_ipc ./../bin/saxpy_many $i $NEVAL
+		bsub -n 1 -o ../measurements/timing/$i.out -R "select [ngpus>0] rusage[ngpus_shared=1]"  ./../bin/saxpy_high $i $NEVAL  
+		bsub -n 1 -o ../measurements/CUPTI/$i.out -R "select [ngpus>0] rusage[ngpus_shared=1]"  nvprof --metrics achieved_occupancy,dram_read_throughput,dram_utilization,dram_write_throughput,l2_read_throughput,l2_write_throughput,issued_ipc ./../bin/saxpy_high $i $NEVAL
 
 	done
 
--- src/saxpy.cu
+++ src/saxpy.cu
@@ -15,28 +15,28 @@ __global__ void saxpy(int n, float a, float *x, float *y)
 }
 
 double min(double* array, int size){
-    // returns the minimum value of array
-    double val = array[0];
-    for (int i = 1; i < size; ++i){
-        val = val <= array[i] ? val : array[i];
-    }
-    return val;
+	// returns the minimum value of array
+	double val = array[0];
+	for (int i = 1; i < size; ++i){
+		val = val <= array[i] ? val : array[i];
+	}
+	return val;
 }
 
 double max(double* array, int size){
-    // returns the maximum value of array
-    double val = array[0];
-    for (int i = 1; i < size; ++i){
-        val = val >= array[i] ? val : array[i];
-    }
-    return val;
+	// returns the maximum value of array
+	double val = array[0];
+	for (int i = 1; i < size; ++i){
+		val = val >= array[i] ? val : array[i];
+	}
+	return val;
 }
 
 double mean(double* array, int size) {
-    double sum=0;
-    for(int i=0; i<size; i++)
-        sum+=array[i];
-    return((double)sum/size);
+	double sum=0;
+	for(int i=0; i<size; i++)
+		sum+=array[i];
+	return((double)sum/size);
 }
 
 int main(int argc, char * argv[])
@@ -46,6 +46,38 @@ int main(int argc, char * argv[])
 	unsigned long int nruns = atoi(argv[2]);
 	size_t size = N*sizeof(float);
 	printf("N = %d\n",N);
+	float *x, *y;		// Host vectors
+	float *d_x, *d_y;	// Device vectors
+
+	// Allocate host memory
+	x = (float *)malloc(size);
+	y = (float *)malloc(size);
+
+	// Allocate device memory
+	cudaMalloc(&d_x, size);
+	cudaMalloc(&d_y, size);
+
+	for (unsigned long int i = 0; i < N; i++){
+		x[i] = 1.0f;
+		y[i] = 2.0f;
+	}
+
+	cudaMemcpy(d_x, x, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_y, y, size, cudaMemcpyHostToDevice);
+
+	// Perform SAXPY on 1M elements
+
+	saxpy<<<(N+255)/256, 256>>>(N, 2.0, d_x, d_y);
+
+
+	cudaMemcpy(y, d_y, size, cudaMemcpyDeviceToHost);
+
+	cudaDeviceSynchronize();
+	cudaFree(d_x);
+	cudaFree(d_y);
+	free(x);
+	free(y);	
+
 	double ntime[nruns],nbandwidth[nruns];
 	for(unsigned long int count = 0; count < nruns; count ++){
 		// Create CUDA events for timing purposes
@@ -53,8 +85,8 @@ int main(int argc, char * argv[])
 		cudaEventCreate(&start);
 		cudaEventCreate(&stop);
 
-		float *x, *y;		// Host vectors
-		float *d_x, *d_y;	// Device vectors
+
+
 
 		// Allocate host memory
 		x = (float *)malloc(size);
@@ -74,9 +106,9 @@ int main(int argc, char * argv[])
 
 		// Perform SAXPY on 1M elements
 		cudaEventRecord(start);
-		saxpy<<<(N+255)/256, 256>>>(N, 2.0, d_x, d_y);
+		saxpy<<<(N+511)/512, 512>>>(N, 2.0, d_x, d_y);
 		cudaEventRecord(stop);
-
+		cudaDeviceSynchronize();
 		cudaMemcpy(y, d_y, size, cudaMemcpyDeviceToHost);
 
 		cudaEventSynchronize(stop);
@@ -88,10 +120,10 @@ int main(int argc, char * argv[])
 		for (unsigned long int i = 0; i < N; i++){
 			maxError = max(maxError, abs(y[i]-4.0f));
 		}
-		printf("Max error: %f\n", maxError);
+		//	printf("Max error: %f\n", maxError);
 		ntime[count] = milliseconds;
 		nbandwidth[count] = N*4*3/milliseconds/1e6;
-	        cudaFree(d_x);
+		cudaFree(d_x);
 		cudaFree(d_y);
 		free(x);
 		free(y);	
